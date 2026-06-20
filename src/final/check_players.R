3e7be9aee9e5541ff186f0a764cc151da20f4848
Vincent Labatut <vince.labatut@gmail.com>
2026-06-20 22:15:02 +0200

    before updating player table

--- src/final/check_players.R
+++ src/final/check_players.R
@@ -1,11 +1,11 @@
 ########################################################################
-# Performs various checks and verification on the three data tables, in
+# Performs various checks and verification on the players data table, in
 # order to detect issues in the data.
 #
 # 06/2025 Vincent Labatut
 #
 # setwd("D:/Users/Vincent/eclipse/workspaces/Test/RugbyScope")
-# source("src/final/check_values.R")
+# source("src/final/check_players.R")
 ########################################################################
 library("stringi")
 library("stringr")
@@ -21,37 +21,24 @@ source("src/common/logging.R")
 
 ########################################################################
 # start logging
-start.rec.log("CheckValues")
-
-
-
-
-########################################################################
-# paths
-data_folder <- file.path("data", "fusion")
+start.rec.log("CheckPlayers")
 
 
 
 
 ########################################################################
 # load tables
-tlog("Loading cleaned tables")
+tlog("Loading player table")
 
-teams <- read.csv(file.path(data_folder, "teams_08.csv"))
-tlog(2, "Number of teams: ", nrow(teams))
+data_folder <- file.path("data", "fusion")
 
 players <- read.csv(file.path(data_folder, "players_08.csv"))
 tlog(2, "Number of players: ", nrow(players))
-
-stints <- read.csv(file.path(data_folder, "stints_16.csv"))
-tlog(2, "Number of stints: ", nrow(stints))
 
 
 
 
 ########################################################################
-# check players
-
 # names: complement missing firstnames / lastnames
 # "fullName" "firstName" "lastName"
 
@@ -69,14 +56,14 @@ fullnames_length <- sapply(fullnames, length)
 split_firsts <- rep(NA, nrow(players))
 split_lasts <- rep(NA, nrow(players))
 
-# split fullnames to recover missing fistnames and lastnames
+# leverage fullnames to recover missing firstnames and lastnames
 # first for fullnames containing 2 words
 idx <- which(fullnames_length == 2 & (is.na(players[, "firstName"]) | is.na(players[, "lastName"])))
 #print(players[idx, c("fullName", "firstName", "lastName", "altNames")])
 split_firsts[idx] <- sapply(fullnames[idx], function(x) x[1])
 split_lasts[idx] <- sapply(fullnames[idx], function(x) x[2])
 print(cbind(players[idx, "fullName"], split_firsts[idx], split_lasts[idx]))
-# then for fullnames containing 3 words
+# second for fullnames containing 3 words
 idx <- which(fullnames_length > 2 & (is.na(players[, "firstName"]) | is.na(players[, "lastName"])))
 #print(players[idx, c("fullName", "firstName", "lastName", "altNames")])
 particles <- c("da", "dal", "dalla", "de", "del", "den", "des", "di", "du", "el", "la", "le", "san", "st.", "te", "van", "vanden", "von")
@@ -121,30 +108,14 @@ before <- players[idx, "lastName"]
 players[idx, "lastName"] <- sapply(fullnames[idx], function(x) x[2])
 print(cbind(players[idx, "fullName"], before, players[idx, "lastName"]))
 
-
-
-
-# record players table
-# tab.file <- file.path(data_folder, "players_09.csv")
-# write.csv(players, tab.file, row.names = FALSE, fileEncoding = "UTF-8")
-
+# check if hyphens are correctly handled
+#idx <- which(grepl("-", players[, "fullName"]) & (grepl(";", players[, "firstName"]) | grepl(";", players[, "lastName"])))
+#print(players[idx, c("fullName", "firstName", "lastName", "altNames")])
 
 
 
 
-
-
-
-
-# TODO
-# noms composés français deviennent 2 noms
-# synch names in player and stint tables
-
-
-
-
-
-
+########################################################################
 # alt names: remove redundant forms and sort remaining forms
 # "altNames"
 split_altnames <- strsplit(players[, "altNames"], ";", fixed = TRUE)
@@ -154,11 +125,13 @@ for (p in 1:nrow(players)) {
   if (!all(is.na(split_altnames[[p]])))
     temp[p] <- paste0(sort(setdiff(split_altnames[[p]], players[p, "fullName"])), collapse = "; ")
 }
+
 # verification
 idx <- which(players[, "altNames"] != temp)
 print(length(idx))
 if (length(idx) > 0)
   for (i in idx) print(c(players[i, "fullName"], players[i, "altNames"], temp[i]))
+
 # replace "" by NA
 temp[temp == ""] <- NA
 # update table
@@ -167,13 +140,15 @@ players[, "altNames"] <- temp
 
 
 
-# dates: check birth < death
+########################################################################
+# dates: check birth < death and so on
 # "birthDate" "deathDate" "careerStartYears" "careerEndYears"
+
 head(sort(unique(players[, "birthDate"])))
 tail(sort(unique(players[, "birthDate"])))
 head(sort(unique(players[, "deathDate"])))
 tail(sort(unique(players[, "deathDate"])))
-idx <- which(players[, "birthDate"] >= players[, "deathDate"])
+idx <- which(players[, "birthDate"] > players[, "deathDate"])
 if (length(idx) > 0)
   print(players[idx, ])
 #
@@ -181,7 +156,7 @@ head(sort(unique(players[, "careerStartYears"])))
 tail(sort(unique(players[, "careerStartYears"])))
 head(sort(unique(players[, "careerEndYears"])))
 tail(sort(unique(players[, "careerEndYears"])))
-idx <- which(players[, "careerStartYears"] >= players[, "careerEndYears"])
+idx <- which(players[, "careerStartYears"] > players[, "careerEndYears"])
 if (length(idx) > 0)
   print(players[idx, ])
 #
@@ -195,8 +170,44 @@ if (length(idx) > 0)
 
 
 
-# places: check that no coma remains
+########################################################################
+# places: clean up a bit, check no comma remains
 # "birthPlaces" "deathPlaces"
+
+# try to clean roughly the place order (in the same entry)
+split_places <- strsplit(c(players[, "birthPlaces"], players[, "deathPlaces"]), "[,;] *")
+split_places_length <- sapply(split_places, length)
+place_cat1 <- c()
+place_cat2 <- c()
+place_cat3 <- c()
+place_others <- c()
+for (i in 1:nrow(players)) {
+  if (!is.na(split_places_length[i])) {
+    if (split_places_length[i] == 1)
+      place_cat1 <- c(place_cat1, split_places[[i]])
+    else if (split_places_length[i] == 2) {
+      place_cat1 <- c(place_cat1, split_places[[i]][1])
+      place_cat2 <- c(place_cat2, split_places[[i]][2])
+    }
+    else if (split_places_length[i] == 3) {
+      place_cat1 <- c(place_cat1, split_places[[i]][1])
+      place_cat2 <- c(place_cat2, split_places[[i]][2])
+      place_cat3 <- c(place_cat3, split_places[[i]][3])
+    } else {
+      place_others <- c(place_others, paste0(split_places[[i]], collapse = "; "))
+    }
+  }
+}
+print(sort(unique(place_cat1)))
+print(sort(unique(place_cat2)))
+print(sort(unique(place_cat3)))
+print(sort(unique(place_others)))
+
+print(sort(intersect(place_cat1, place_cat2)))
+print(sort(intersect(place_cat1, place_cat3)))
+print(sort(intersect(place_cat2, place_cat3)))
+
+# replace remaining commas by semicolons
 idx <- which(grepl(",", players[, "birthPlaces"], fixed = TRUE))
 if (length(idx) > 0)
   print(players[idx, c("wikidataId", "birthPlaces")])
@@ -210,133 +221,163 @@ players[, "deathPlaces"] <- gsub(",", ";", players[, "deathPlaces"], fixed = TRU
 
 
 
+########################################################################
 # countries: verify normalization
 # "citizenships" "sportCountries"
 print(sort(unique(trimws(unlist(strsplit(players[, "citizenships"], ";"))))))
+print(sort(unique(trimws(unlist(strsplit(players[, "sportCountries"], ";"))))))
 
 
 
 
+########################################################################
 # positions: verify unicity and normalization
 # "positions"
+split_pos <- strsplit(players[, "positions"], ";")
+print(sort(unique(trimws(unlist(split_pos)))))
+
+# # sort positions
+# pos_str <- sapply(1:nrow(players), function(i) if (!all(is.na(split_pos[[i]]))) paste0(sort(unique(split_pos[[i]])), collapse = "; ") else NA)
+# idx <- which(pos_str != players[, "positions"])
+# print(cbind(players[idx, "positions"], pos_str[idx]))
+# finally, no: keep original order from WP page, as it may have a meaning
 
 
 
 
+########################################################################
 # weight/height: check consistency
 # "weights" "heights"
+print(sort(unique(players[, "weights"])))
+print(sort(unique(players[, "heights"])))
 
 
 
 
+########################################################################
 # ids: check unicity
 # "wikidataId" "espnScrumIds" "allRugbyIds" "googleKnowlIds" "itsRugbyIds" "rugbyDatabaseIds" "dbpediaId"
+id_field <- "wikidataId"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
+
+id_field <- "espnScrumIds"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
+
+id_field <- "allRugbyIds"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
+
+id_field <- "googleKnowlIds"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
+
+id_field <- "itsRugbyIds"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
+# also convert to integers
+before <- players[, "itsRugbyIds"]
+after <- as.integer(players[, "itsRugbyIds"])
+which(!is.na(before) & is.na(after))
+players[, "itsRugbyIds"] <- after
+
+id_field <- "rugbyDatabaseIds"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
+
+id_field <- "dbpediaId"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
 
 
 
 
+########################################################################
 # urls: check unicity
 # "wikipediaEn" "wikipediaFr" "wikipediaIt" "wikipediaEs" "wikipediaJa"
+id_field <- "wikipediaEn"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
+
+id_field <- "wikipediaFr"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
+
+id_field <- "wikipediaIt"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
+
+id_field <- "wikipediaEs"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
+
+id_field <- "wikipediaJa"
+tlog(2, "Processing ", id_field)
+tt <- table(players[, id_field])
+duplicates <- names(tt[tt > 1])
+if (length(duplicates) > 0)
+  print(players[players[, id_field] %in% duplicates, ])
 
 
 
 
 ########################################################################
-# check teams
+# adjust column names
+colnames[colnames(players) == "firstName"]  <- "firstNames"
+colnames[colnames(players) == "lastName"]  <- "lastNames"
+#
+colnames[colnames(players) == "weights"]  <- "weight"
+colnames[colnames(players) == "heights"]  <- "height"
+#
+colnames[colnames(players) == "careerStartYears"]  <- "careerStartYear"
+colnames[colnames(players) == "careerEndYears"]  <- "careerEndYear"
+#
+colnames[colnames(players) == "espnScrumIds"]  <- "espnScrumId"
+colnames[colnames(players) == "allRugbyIds"]  <- "allRugbyId"
+colnames[colnames(players) == "googleKnowlIds"]  <- "googleKnowlId"
+colnames[colnames(players) == "itsRugbyIds"]  <- "itsRugbyId"
+colnames[colnames(players) == "rugbyDatabaseIds"]  <- "rugbyDatabaseId"
 
-# ids: check unicity
-# "rugbyscopeId" "wikidataId" "allRugbyIds" "googleKnowlIds" "dbpediaId"
+# record players table
+tab.file <- file.path(data_folder, "players_09.csv")
+write.csv(players, tab.file, row.names = FALSE, fileEncoding = "UTF-8")
 
-
-
-
-# alt names: remove redundant names and sort remaining names
-# "fullName" "altNames"
-
-
-
-
-# categories: verify normalization
-# "type" "affiliations" "countries" "competitions" "tier"
-
-
-
-
-# numeric: check formating
-# "homeVenueCapacities"
-
-
-
-
-# place names: nothing to do
-# "homeVenueNames" "locations"
-
-
-
-
-# dates: check inception < termination
-# "inceptionDate" "terminationDate"
-
-
-
-
-# urls: check unicity
-# "wikipediaEn" "wikipediaFr" "wikipediaIt" "wikipediaEs" "wikipediaJa"
-
-
-
-
-# comments: nothing to do
-# "comments"
-
-
-
-
-########################################################################
-# check stints
-
-# player: check existence in player table and name consistency
-# "playerId" "playerName"
-
-
-
-
-# categories: check normalization
-# "type"
-
-
-
-
-# team: check existence in team table and name consistency
-# "teamWdId" "teamRsId" "teamName"
-
-
-
-
-# dates: check start < end
-# "startYear" "endYear"
-
-
-
-
-# stats: check consistency
-# "matchesPlayed" "pointsScored"
-
-
-
-
-# source: sort and remove redundant values, check normalization
-# "dataSource"
-
-
-
-
-########################################################################
 # stop logging
 end.rec.log()
 
-
-
 # TODO
-# firstName => firstNames
-# lastName => lastNames
+# synch names in player and stint tables
